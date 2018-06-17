2a4d0da2d7de22f9f562095c3aeb0b44d09a2887
Alessandro Koterle <alessandro.koterle@gmail.com>
2018-06-17 10:48:15 +0200

    first commit

---
+++ src/main/kotlin/GolGrid.kt
@@ -0,0 +1,87 @@
+import java.util.*
+
+typealias IntMatrix = MutableList<MutableList<Int>>
+
+open class GolGrid(private val rows: Int,
+                   private val cols: Int,
+                   initialState: List<Int> = listOf()) {
+
+    private var grid: IntMatrix = mutableListOf()
+
+    val currentState: List<Int>
+        get() = grid.flatten()
+
+
+    init {
+        val fillState = when (initialState.size == rows * cols) {
+            true -> initialState
+            else -> with(Random()) {
+                (1..rows * cols).map { nextInt(2) }
+            }
+        }
+        createGrid()
+        fillGridUp(fillState)
+    }
+
+    fun nextGeneration(): List<List<Int>> {
+        (0 until (rows * cols))
+                .map { Pair(it / cols % rows, it % cols) }
+                .map { applyRules(it.first, it.second) }
+                .run(::fillGridUp)
+
+        return grid.map(MutableList<Int>::toList)
+
+    }
+
+
+    private fun applyRules(row: Int, col: Int): Int {
+        val (aliveNeighbors, _) = neighbors(row, col)
+                .map { grid[it.first][it.second] }
+                .partition { state -> state == 1 }
+
+        val alive = grid[row][col] == 1
+
+        return when (alive) {
+            true -> when (aliveNeighbors.count()) {
+                0, 1 -> 0
+                2, 3 -> 1
+                else -> 0
+            }
+            else -> when (aliveNeighbors.count()) {
+                3 -> 1
+                else -> 0
+            }
+        }
+    }
+
+    private fun createGrid() = (1..rows * cols)
+            .map { Pair(it / cols % rows, it % cols) }
+            .groupBy({ it.first }, { it.second })
+            .forEach { grid.add(it.key, it.value.toMutableList()) }
+
+    private fun fillGridUp(state: List<Int>) =
+            state.mapIndexed { index, i ->
+                val row = index / cols % rows
+                val col = index % cols
+                grid[row][col] = i
+            }
+
+
+    private fun neighbors(x: Int, y: Int): List<Pair<Int, Int>> {
+
+        val neighbors: MutableList<Pair<Int, Int>> = mutableListOf()
+
+        if (y + 1 < cols) neighbors.add(Pair(x, y + 1))
+        if (y > 0) neighbors.add(Pair(x, y - 1))
+        if (x + 1 < rows) neighbors.add(Pair(x + 1, y))
+        if (x > 0) neighbors.add(Pair(x - 1, y))
+        if ((x + 1 < rows) and (y + 1 < cols)) neighbors.add(Pair(x + 1, y + 1))
+        if ((x + 1 < rows) and (y > 0)) neighbors.add(Pair(x + 1, y - 1))
+        if ((x > 0) and (y + 1 < cols)) neighbors.add(Pair(x - 1, y + 1))
+        if ((x > 0) and (y > 0)) neighbors.add(Pair(x - 1, y - 1))
+
+        return neighbors
+
+    }
+
+}
---
+++ src/test/kotlin/GameOfLifeTests.kt
@@ -0,0 +1,122 @@
+import org.junit.Assert
+import org.junit.Test
+
+class GameOfLifeTests {
+
+    @Test
+    fun `a live cell with zero or one live neighbours will die`() {
+
+        GolGrid(3, 3, listOf(
+                0, 0, 0,
+                0, 1, 0,
+                0, 0, 0)) //given
+                .run { nextGeneration() } //when
+                .let { state -> Assert.assertEquals(0, state[1][1]) } //then
+
+        GolGrid(3, 3, listOf(
+                1, 0, 0,
+                0, 1, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+
+        GolGrid(3, 3, listOf(
+                0, 0, 0,
+                0, 1, 0,
+                0, 0, 1))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+
+        GolGrid(3, 3, listOf(
+                0, 0, 0,
+                0, 1, 0,
+                1, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+    }
+
+    @Test
+    fun `a live cell with two or three live neighbours will remain alive`() {
+        GolGrid(3, 3, listOf(
+                1, 1, 0,
+                0, 1, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(1, state[1][1]) }
+        GolGrid(3, 3, listOf(
+                1, 1, 0,
+                1, 1, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(1, state[1][1]) }
+
+
+        GolGrid(3, 3, listOf(
+                0, 1, 0,
+                1, 1, 0,
+                0, 1, 0)).run { nextGeneration() }
+                .let { state -> Assert.assertEquals(1, state[1][1]) }
+    }
+
+    @Test
+    fun `a live cell with four or more live neighbours will die`() {
+        GolGrid(3, 3, listOf(
+                1, 1, 1,
+                1, 1, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+
+        GolGrid(3, 3, listOf(
+                1, 1, 1,
+                1, 1, 1,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+        GolGrid(3, 3, listOf(
+                1, 1, 1,
+                1, 1, 1,
+                1, 1, 1))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+    }
+
+    @Test
+    fun `a dead cell with exactly three live neighbours becomes alive`() {
+        GolGrid(3, 3, listOf(
+                0, 1, 1,
+                0, 0, 1,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(1, state[1][1]) }
+        GolGrid(3, 3, listOf(
+                0, 1, 0,
+                1, 0, 0,
+                0, 1, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(1, state[1][1]) }
+    }
+
+    @Test
+    fun `in all other cases a dead cell will stay dead`() {
+        GolGrid(3, 3, listOf(
+                0, 1, 1,
+                0, 0, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+        GolGrid(3, 3, listOf(
+                1, 0, 1,
+                0, 0, 0,
+                0, 0, 0))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+        GolGrid(3, 3, listOf(
+                0, 0, 0,
+                0, 0, 0,
+                0, 1, 1))
+                .run { nextGeneration() }
+                .let { state -> Assert.assertEquals(0, state[1][1]) }
+
+    }
+}
